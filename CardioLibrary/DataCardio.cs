d438b1588511574c4a290e096711710c7258fb17
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2019-04-04 10:13:32 +0200

    punto 1 parzialmente completato

--- CardioLibrary/DataCardio.cs
+++ CardioLibrary/DataCardio.cs
@@ -8,5 +8,21 @@ namespace CardioLibrary
 {
     public class DataCardio
     {
+        public static double battiti_Min (int età)
+        {
+            int frequenza = 220 - età;
+           double battiti_Min = frequenza * 0.7;
+            return battiti_Min;
+        }
+
+        public static double battiti_Max(int età)
+        {
+            int frequenza = 220 - età;
+            double battiti_Max = frequenza * 0.9;
+            return battiti_Max;
+        }
+
+
+
     }
 }
--- DataCardio.Test/UnitTest1.cs
+++ DataCardio.Test/UnitTest1.cs
@@ -6,10 +6,17 @@ namespace DataCardio.Test
     [TestClass]
     public class UnitTest1
     {
-        [TestMethod]
-        public void TestMethod1()
+        [DataTestMethod]
+        [DataRow(14, 206, 144.2, 185.4)]        // età,frequenza,battiti minimi,battiti massimi
+        [DataRow(17, 203, 142.1, 182.7)]                    // per i battiti minimi frequenza * 0.7
+        [DataRow(25, 195, 136.5, 175.5)]                   // per i battiti massimi frequenza * 0.9
+        [DataRow(30, 190, 133, 171)]
+        [DataRow(50, 170, 119, 153)]
+        public void TestMethod1(int età, int frequenza, double battiti_Min_apettati, double battiti_Max_aspettati)                       )
         {
 
+           double battiti_Min_aspettati = CardioLibrary.DataCardio.battiti_Min(età);
+
         }
-    }
+    } 
 }
